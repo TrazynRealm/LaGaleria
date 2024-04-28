205c9606fb9d0fddf895903d65dbea957e7a3c51
Efe Rojas <rojasdsa@alumnos.ilernasevilla.com>
2024-04-28 20:44:40 +0200

    Primeros pasos: Header y body

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
   content: ["./src/**/*.{html,js,ejs}"],
   mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        for: {
+          pri: '#000000', // Negro
+          sec: '#3D3D3D', // Gris oscuro
+          acc: '#FFA500', // Naranja
+        },
+        sto: {
+          pri: '#8B4513', // Marrón oscuro
+          sec: '#556B2F', // Verde oliva
+          acc: '#CD5C5C', // Rojo índigo
+        },
+      },
+    },
   },
   plugins: [],
 }
