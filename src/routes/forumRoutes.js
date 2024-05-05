7f23df4e1143585966234a464fc5f6e260975483
fcorguez92 <fcorguezsevilla@gmail.com>
2024-05-05 03:07:30 +0200

    postlists w/mongo

--- src/routes/forumRoutes.js
+++ src/routes/forumRoutes.js
@@ -3,16 +3,18 @@ const router = express.Router();
 const forumController = require('../controllers/forumController'); 
 
 // Ruta para mostrar la página principal del foro
-router.get('/forum', (req, res) => {
-    const categories = forumController.getForumCategories(); // Obtén las categorías del foro
-    const posts = forumController.getLatestPosts(); // Obtén los últimos temas del foro
-    res.render('forum/index', { categories, posts }); // Renderiza la vista y pasa las categorías y los temas como datos
-});
+// router.get('/forum', (req, res) => {
+//     const categories = forumController.getForumCategories(); // Obtén las categorías del foro
+//     const posts = forumController.getLatestPosts(); // Obtén los últimos temas del foro
+//     res.render('forum/index', { categories, posts }); // Renderiza la vista y pasa las categorías y los temas como datos
+// });
+
+router.get('/forum', forumController.getAllPosts);
 
 router.get('/forum/post/:postId', (req, res) => {
     const postId = req.params.postId;
     const post = forumController.getPostById(postId); // Corregir aquí
-    const comments = forumController.getComments(topicId);
+    const comments = forumController.getComments(postId);
     res.render('forum/post', { post, comments }); // Renderizar la vista y pasar el tema como datos
 });
 
