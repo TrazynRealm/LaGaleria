c3f694c98aafd487d3992cfcd2389da1d8aa1213
fcorguez92 <fcorguezsevilla@gmail.com>
2024-05-05 22:29:01 +0200

    Reorganización foro

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('../config/passport-config'); // Importa el middleware de autenticación
+// const passport = require('../config/passport-config'); // Importa el middleware de autenticación
 
 const productRoutes = require('./productRoutes');
 const forumRoutes = require('./forumRoutes');
 const userRoutes = require('./userRoutes')
 
 // Middleware de autenticación aplicado solo a rutas específicas
-// router.use('/forum', passport.authenticate('local'));
+// router.use('/forum/new-post', passport.authenticate('local'));
 
 router.use('/', userRoutes);
 router.use(productRoutes);
