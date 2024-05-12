69faeba43ce126c3d59f16d458da8d282448340a
fcorguez92 <fcorguezsevilla@gmail.com>
2024-05-12 13:31:36 +0200

    Checkpoint

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -125,3 +125,23 @@ exports.deleteProduct = async (req, res) => {
         res.redirect('/admin/products'); // Redirige a la página de productos en caso de error
     }
 };
+
+exports.createProduct = async (req, res) => {
+    try {
+        const product = new Product(req.body);
+        await product.save();
+        res.status(201).send(product);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
+
+exports.updateProduct = async (req, res) => {
+    try {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        if (!product) return res.status(404).send('El producto no existe.');
+        res.send(product);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
